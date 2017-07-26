1a1fba7236ecb25dd25ed0b80be1415b16ea1c02
Charles Julian Knight <julian@fixdapp.com>
2017-07-25 20:34:08 -0400

    refactor conversion code into function

--- index.js
+++ index.js
@@ -5,24 +5,43 @@ var REALTIME_TRAIN_ENDPOINT = 'http://developer.itsmarta.com/RealtimeTrain/RestS
 var REALTIME_BUS_ALL_ENDPOINT = 'http://developer.itsmarta.com/BRDRestService/RestBusRealTimeService/GetAllBus'
 var REALTIME_BUS_ROUTE_ENDPOINT = 'http://developer.itsmarta.com/BRDRestService/RestBusRealTimeService/GetBusByRoute'
 
+function convertTrainArrival (arrival) {
+  return {
+    destination: arrival['DESTINATION'],
+    direction: arrival['DIRECTION'],
+    event_time: moment(arrival['EVENT_TIME'], 'M/D/YYYY h:mm:ss A'),
+    line: arrival['LINE'],
+    next_arr: moment(arrival['EVENT_TIME'], 'h:mm:ss A'),
+    station: arrival['STATION'],
+    train_id: arrival['TRAIN_ID'],
+    waiting_seconds: parseInt(arrival['WAITING_SECONDS'], 10),
+    waiting_time: arrival['WAITING_TIME']
+  };
+}
+
+function convertBusArrival (arrival) {
+  return {
+    adherence: parseInt(arrival['ADHERENCE'], 10),
+    block_id: arrival['BLOCKID'],
+    block_abbr: arrival['BLOCK_ABBR'],
+    direction: arrival['DIRECTION'],
+    latitude: parseFloat(arrival['LATITUDE']),
+    longitude: parseFloat(arrival['LONGITUDE']),
+    msg_time: moment(arrival['MSGTIME'], 'M/D/YYYY h:mm:ss A'),
+    route: arrival['ROUTE'],
+    stop_id: arrival['STOPID'],
+    timepoint: arrival['TIMEPOINT'],
+    trip_id: arrival['TRIPID'],
+    vehicle: arrival['VEHICLE']
+  };
+}
+
 module.exports.getRealtimeTrainArrivals = function (apiKey, callback) {
   request(REALTIME_TRAIN_ENDPOINT + '?apikey=' + apiKey, function (error, response, body) {
     if (error) {
       callback(error, null);
     } else {
-      var arrivals = JSON.parse(body).map(function (arrival) {
-        return {
-          destination: arrival['DESTINATION'],
-          direction: arrival['DIRECTION'],
-          event_time: moment(arrival['EVENT_TIME'], 'M/D/YYYY h:mm:ss A'),
-          line: arrival['LINE'],
-          next_arr: moment(arrival['EVENT_TIME'], 'h:mm:ss A'),
-          station: arrival['STATION'],
-          train_id: arrival['TRAIN_ID'],
-          waiting_seconds: parseInt(arrival['WAITING_SECONDS'], 10),
-          waiting_time: arrival['WAITING_TIME']
-        }
-      });
+      var arrivals = JSON.parse(body).map(convertTrainArrival);
       callback(null, arrivals);
     }
   });
@@ -33,22 +52,7 @@ module.exports.getAllRealtimeBusArrivals = function (callback) {
     if (error) {
       callback(error, null);
     } else {
-      var arrivals = JSON.parse(body).map(function (arrival) {
-        return {
-          adherence: parseInt(arrival['ADHERENCE'], 10),
-          block_id: arrival['BLOCKID'],
-          block_abbr: arrival['BLOCK_ABBR'],
-          direction: arrival['DIRECTION'],
-          latitude: parseFloat(arrival['LATITUDE']),
-          longitude: parseFloat(arrival['LONGITUDE']),
-          msg_time: moment(arrival['MSGTIME'], 'M/D/YYYY h:mm:ss A'),
-          route: arrival['ROUTE'],
-          stop_id: arrival['STOPID'],
-          timepoint: arrival['TIMEPOINT'],
-          trip_id: arrival['TRIPID'],
-          vehicle: arrival['VEHICLE']
-        }
-      });
+      var arrivals = JSON.parse(body).map(convertBusArrival);
       callback(null, arrivals);
     }
   });
@@ -59,22 +63,7 @@ module.exports.getRealtimeBusArrivalsByRoute = function (route, callback) {
     if (error) {
       callback(error, null);
     } else {
-      var arrivals = JSON.parse(body).map(function (arrival) {
-        return {
-          adherence: parseInt(arrival['ADHERENCE'], 10),
-          block_id: arrival['BLOCKID'],
-          block_abbr: arrival['BLOCK_ABBR'],
-          direction: arrival['DIRECTION'],
-          latitude: parseFloat(arrival['LATITUDE']),
-          longitude: parseFloat(arrival['LONGITUDE']),
-          msg_time: moment(arrival['MSGTIME'], 'M/D/YYYY h:mm:ss A'),
-          route: arrival['ROUTE'],
-          stop_id: arrival['STOPID'],
-          timepoint: arrival['TIMEPOINT'],
-          trip_id: arrival['TRIPID'],
-          vehicle: arrival['VEHICLE']
-        }
-      });
+      var arrivals = JSON.parse(body).map(convertBusArrival);
       callback(null, arrivals);
     }
   });
